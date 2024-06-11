22e80b5d3ba7ee7099525d2b77e3b1e7aa63c60e
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-06-11 08:18:28 +0700

    ADD - Handler CRUD Note

--- models/note.model.js
+++ models/note.model.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const NoteModel = new mongoose.Schema({
-   title: {
+const NoteModel = new mongoose.Schema(
+  {
+    title: {
       type: String,
       required: true,
-   },
-   content: {
+    },
+    content: {
       type: String,
       required: true,
-   },
-   user_id: {
+    },
+    user_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-   },
-});
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Note", NoteModel);
